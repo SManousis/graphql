c40b76736955779d4d429188e9ebd7cf3c25d075
smanousi <lookoptica@otenet.gr>
2025-10-13 16:28:11 +0300

    1st commit

---
+++ proxy/handlers.go
@@ -0,0 +1,21 @@
+package proxy
+
+import "net/http"
+
+func authHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+		if r.Method != http.MethodPost {
+			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+	}
+}
+
+func graphqlHandler(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ proxy/models.go
@@ -0,0 +1,12 @@
+package proxy
+
+type loginRequest struct {
+	Identity string `json:"identity"` // username OR email
+	Password string `json:"password"`
+}
+
+type loginResponse struct {
+	Token string `json:"token"`
+	// optional: include expiry if the auth API returns it
+	Exp *int64 `json:"exp,omitempty"`
+}
