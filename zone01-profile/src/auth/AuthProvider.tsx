fc55b1a0307245fcec79dad0133d9c00cbaa4677
Smanousis <lookoptica@otenet.gr>
2025-10-28 14:16:16 +0200

    removed react from import

--- zone01-profile/src/auth/AuthProvider.tsx
+++ zone01-profile/src/auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { signin } from "../lib/api";
 import { AuthContext, type AuthCtx } from "./context";
 
--- zone01-profile/src/pages/LoginPage.tsx
+++ zone01-profile/src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useAuth } from "../auth/useAuth";
 import { messageFromError } from "../lib/errors";
 
